3afad115ece1506dfab2bff16f908a942c17928f
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-06-29 10:48:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const { Client } = require('discord.js');
 const bot = new Client({ intents: 33283 });
 const { PSClient } = require('psc');
-const psc = new PSClient({ client: bot, prefix: "." });
+const psc = new PSClient({ client: bot, prefix: "?" });
 
 const { Soup } = require('stews');
 
 const config = require('./config.json');
-
-
-bot.on('ready', async () => {
-	console.log(`Logged in as ${bot.user.tag}`);
-});
-
 
 let compiles = Soup.from(config.compile);
 
@@ -19,10 +13,8 @@ compiles = compiles.map( (call, dir) => {
 	return psc.compile(dir);
 });
 
-
 let stuff = Soup.from({ psc, bot });
 let exp = stuff.merge(compiles);
-
 
 module.exports = exp;
 config.build.forEach( (dir) => {
