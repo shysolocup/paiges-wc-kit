9cc61e985f6b099a6a49bc3b7f0dac9cb4d438ee
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-25 12:51:17 -0500

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,14 +1,14 @@
-var { psc, bot, assets } = require('../../index.js');
+var { wc, bot, assets } = require('../../index.js');
 var { link } = assets;
 
 async function action(ctx, cmd) {
-	let embed = new psc.Embed({
-		description: `### [Paige's PSC Kit](${link}) \n Hi! Welcome to Paige's PSC Kit!\n If you have any questions you can put it on the github page above`,
+	let embed = new wc.Embed({
+		description: `### [Paige's WillClient Kit](${link}) \n Hi! Welcome to Paige's WillClient Kit!\n If you have any questions you can put it on the github page above`,
 
-		color: psc.colors.blurple
+		color: wc.colors.blurple
 	});
 
 	ctx.reply({ embeds: [embed] });
 }
 
-psc.command("help", action);
+wc.command("help", action);
