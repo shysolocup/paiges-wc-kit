286c0e2349401ee675461311c99715a2f0509344
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-25 12:48:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const config = require('./config.json');
 
 const { Client } = require('discord.js');
 const bot = new Client({ intents: config.intents });
-const { PSClient } = require('psc');
-const psc = new PSClient({ client: bot, prefix: config.prefix });
+const { WillClient } = require('willclient');
+const wc = new PSClient({ client: bot, prefix: config.prefix });
 
 const { Soup } = require('stews');
 
 let compiles = Soup.from(config.compile);
 
 compiles = compiles.map( (call, dir) => {
-	return psc.compile(dir);
+	return wc.compile(dir);
 });
 
-let stuff = Soup.from({ psc, bot });
+let stuff = Soup.from({ wc, bot });
 let exp = stuff.merge(compiles);
 
 module.exports = exp;
 config.build.forEach( (dir) => {
-	psc.build(dir);
+	wc.build(dir);
 });
 
 
