34107019fdf90f90053a629f0804aaabb40a07a1
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-25 12:50:33 -0500

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,30 +1,30 @@
-var { psc, bot } = require('../../index.js');
+var { wc, bot } = require('../../index.js');
 
 async function action(ctx, cmd) {
 	if (cmd.onCooldown) {
-			return psc.reply(
+			return wc.reply(
 				`You can use this command again ${cmd.cooldown.relative}`, 
 				{ deleteAfter: "1s" }
 			);
 	}
 	
-	let user = (cmd.args[0]) ? await psc.fetchUser(cmd.args[0]) : ctx.author;
+	let user = (cmd.args[0]) ? await wc.fetchUser(cmd.args[0]) : ctx.author;
 	if (!user) user = ctx.author;
 
-	let avatar = psc.user.avatarURL(user);
+	let avatar = wc.user.avatarURL(user);
 
 	
-	let embed = new psc.Embed({
+	let embed = new wc.Embed({
 		author: { name: user.username, icon: avatar },
 		
 		description: `### [Avatar](${avatar})`,
 		image: avatar,
 		
-		color: psc.colors.blurple
+		color: wc.colors.blurple
 	});
 
 	
 	ctx.reply({ embeds: [embed] });
 }
 
-psc.command({ name: "avatar", aliases: ["av"], cooldown: "5s" }, action);
+wc.command({ name: "avatar", aliases: ["av"], cooldown: "5s" }, action);
