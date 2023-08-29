09541b3579b256b9afcb9483e7db078600e06f01
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-29 11:03:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const config = require('./config.json');
 
 const { Client } = require('discord.js');
 const bot = new Client({ intents: config.intents });
-const { WillClient } = require('./src/test');
+const { WillClient } = require('willclient');
 const wc = new WillClient({ client: bot, prefix: config.prefix });
 
 const { Soup } = require('stews');
