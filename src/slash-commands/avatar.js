389629b7ad12949fbeca4ea41b99b40be35c015b
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-30 13:22:53 -0500

    Create avatar.js

---
+++ src/slash-commands/avatar.js
@@ -0,0 +1,36 @@
+var { wc, bot } = require('../../index.js');
+
+async function action(ctx, cmd) {
+	if (cmd.onCooldown) {
+			return wc.reply(
+				`You can use this command again ${cmd.cooldown.relative}`, 
+				{ deleteAfter: "1s", ephemeral: true }
+			);
+	}
+	
+	let user = (cmd.args[0]) ? await wc.fetchUser(cmd.args[0].value) : ctx.user;
+	if (!user) user = ctx.user;
+
+	let avatar = wc.user.avatarURL(user);
+
+	
+	let embed = new wc.Embed({
+		author: { name: user.username, icon: avatar },
+		
+		description: `### [Avatar](${avatar})`,
+		image: avatar,
+		
+		color: wc.colors.blurple
+	});
+
+	
+	ctx.reply({ embeds: [embed] });
+};
+
+let options = [{
+    name: "user",
+    description: "User to get the avatar of",
+    type: "user"
+}];
+
+wc.slashCommand({ name: "avatar", desc cooldown: "5s" }, action);
