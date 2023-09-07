96d5c6e9aa41f387ee9a69ac6076770be6cd7a4e
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-07 13:07:35 -0500

    Create help.js

---
+++ src/commands/slash/help.js
@@ -0,0 +1,14 @@
+var { wc, bot, assets } = require('../../../index.js');
+var { link } = assets;
+
+async function action(ctx, cmd) {
+	let embed = new wc.Embed({
+		description: `### [Paige's WillClient Kit](${link}) \n Hi! Welcome to Paige's WillClient Kit!\n If you have any questions you can put it on the github page above`,
+
+		color: wc.colors.blurple
+	});
+
+	ctx.reply({ embeds: [embed] });
+}
+
+wc.slashCommand("help", "sends a help message", action);
